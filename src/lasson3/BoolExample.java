e941aa31fae66ecff40ee30272105901e73725fb
Paruir <paruir.pogosyan.90@mail.ru>
2021-10-03 20:17:27 +0300

    lesson3

--- src/lasson3/BoolExample.java
+++
@@ -1,8 +0,0 @@
-package lasson3;
-
-public class BoolExample {
-    public static void main(String[] args) {
-        boolean a=true; a=false;
-        System.out.println(10>20);
-    }
-}
---
+++ src/lesson3/CharDemo.java
@@ -0,0 +1,9 @@
+package lesson3;
+
+public class CharDemo {
+    public static void main(String[] args) {
+        char c;
+        c = 88;
+        System.out.println(c);
+    }
+}
