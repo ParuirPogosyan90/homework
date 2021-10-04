91c5947dda5d6996ec80f6188aa89803eb35cd2b
Paruir <paruir.pogosyan.90@mail.ru>
2021-10-04 20:07:14 +0300

    Мое 2 задание

---
+++ src/lesson4/LifeTime.java
@@ -0,0 +1,12 @@
+package lesson4;
+
+public class LifeTime {
+    public static void main(String[] args) {
+        int x;
+        for (x=0;x<3;x++)
+            System.out.println(x);
+
+
+
+    }
+}
