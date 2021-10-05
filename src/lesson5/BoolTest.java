24ba747a1e49c247312e056bb0a4fd9af1dceec5
Paruir <paruir.pogosyan.90@mail.ru>
2021-10-05 20:26:53 +0300

    Мое 3 задание

---
+++ src/lesson5/BoolTest.java
@@ -0,0 +1,17 @@
+package lesson5;
+
+public class BoolTest {
+    public static void main(String[] args) {
+        boolean b = false;
+        final int i = 'b' - 66;
+        System.out.println("b=" + b);
+        b = true;
+        System.out.println("b=" + b);
+
+        b = false;
+            System.out.println("10>9=" + (10 > 9));
+
+
+    }
+
+}
