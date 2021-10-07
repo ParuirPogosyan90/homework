94f2c4b472e69a1161a53a424fde132dfddbed83
Paruir <paruir.pogosyan.90@mail.ru>
2021-10-07 19:33:47 +0300

    Мое 5 задание

---
+++ src/lesson7/FigurePainter.java
@@ -0,0 +1,30 @@
+package lesson7;
+
+public class FigurePainter {
+
+    public static void main(String[] args) {
+
+        /**
+         *  *
+         *  * *
+         *  * * *
+         *  * * * *
+         *  * * * * *
+         */
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+    }
+}
