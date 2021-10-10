ab15f1fd3b78330286323771330ae4bd6f8e45ed
Paruir <paruir.pogosyan.90@mail.ru>
2021-10-10 19:15:10 +0300

    Мое 10 задание

---
+++ src/lesson11/IncrementExample.java
@@ -0,0 +1,11 @@
+package lesson11;
+// x=11
+// y=10
+public class IncrementExample {
+    public static void main(String[] args) {
+        int x=10;
+        int y=x++;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+    }
+}
