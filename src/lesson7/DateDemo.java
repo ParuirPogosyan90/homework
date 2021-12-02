522bfe1cb0d85ec9eeeac582942c5db6d86944f7
Paruir <paruir.pogosyan.90@mail.ru>
2021-12-02 20:42:38 +0300

    lesson 20 (02.12.2021)

---
+++ src/lesson7/DateDemo.java
@@ -0,0 +1,23 @@
+package lesson7;
+
+import java.util.Date;
+
+
+public class DateDemo {
+    public static void main(String[] args) {
+        Date date=new Date();
+        System.out.println("\u001B[33m"+date);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
